5709a1b02cd110688f869d57af85b3bc7bb51794
shuka <shuka@pitatech.com>
2022-08-22 07:21:23 +0300

    Add Spring Boot skeleton

---
+++ src/test/java/com/shuka/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuka.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
